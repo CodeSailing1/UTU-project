cab0d4ef681d8cf986e4586695896a274afacf06
Juan Gonzalez <jai.gonz.pan@gmail.com>
2024-07-07 20:04:44 -0300

    Created a new function that desactivate the button submit if the inputs are false

--- interfaz/scripts/validator.js
+++ interfaz/scripts/validator.js
@@ -1,43 +1,70 @@
 const requeriments = {
     name: /^[a-zA-Z\s]{2,25}$/,
     lastName: /^[a-zA-Z\s]{2,25}$/,
-    email: /^[a-zA_.\S]+@[a-z]+\.[a-z]{2,50}$/,
-    password:/^[\w\S-¿?^*´!|#$%&/{}()=><;:,.-]{8,50}$/
+    email:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,50}$/,
+    password:/^[\w\S-¿?^*´!|#$%&/{}()=><;:,.-]{8,50}$/,
+    day: /^[0-9\s]{1,4}$/,
+    month: /^[0-9\s]{1,4}$/,
+    year: /^[0-9\s]{1,4}$/
 }
 const tFs = {
     name: false,
     lastName: false,
     email: false,
-    password:false
+    password:false,
+    day: false,
+    month: false,
+    year: false
 }
 
 const signUpForm = document.getElementById("signUpForm");
 const inputSignUp = document.querySelectorAll("#signUpForm input");
+const submit = document.getElementById("signUp");
 
 inputSignUp.forEach(input => {
     input.addEventListener("input", (e) => {
-        validation(requeriments[e.target.name], e.target.value, e.target.id);
+        let validation = validationInputs(requeriments[e.target.name], e.target.value, e.target.id);
+        validationInputs(requeriments[e.target.name], e.target.value, e.target.id)
+        submitValidation(validation);
     });
-});
+})
 
-function validation(requeriment, input, tF) {
+function validationInputs(requeriment, input, idInput) {
 
-    const element = document.getElementById(tF);
+    const element = document.getElementById(idInput);
     try{
         if(requeriment.test(input)){
-            element.classList.add("valid");
-            element.classList.remove("invalid");
-            tFs[tF] = true;
-        } else{
-            element.classList.add("invalid");
-            element.classList.remove("valid");
-            tFs[tF] = false;
-            throw new Error("Ingresa valores validos");
-        }
+                element.classList.add("valid");
+                element.classList.remove("invalid");
+                tFs[idInput] = true;
+                return true;
+            } else{
+                element.classList.add("invalid");
+                element.classList.remove("valid");
+                tFs[idInput] = false;
+                throw new Error("Ingresa valores validos");
+            }
     }catch(e) {
         if(!requeriment.test(input)){
             console.error(e.message);
+            return false
         }
     }
-    console.log(requeriment.test(input))
 }
+
+function submitValidation(validation) {
+    let allElementsValid = true;
+    inputSignUp.forEach( () => {
+        if (validation) {
+            allElementsValid = true;   
+        } else {
+            allElementsValid = false;   
+        }
+    });
+    if (allElementsValid) {
+        submit.removeAttribute("disabled");
+    } else {
+        submit.setAttribute("disabled", "");
+    }
+}
+
