a75a5b2ad29a0586d02620b511b458fe0d16c35b
Juan Gonzalez <jai.gonz.pan@gmail.com>
2024-07-12 18:44:44 -0300

    Fixed a bug on the button activation script

--- interfaz/scripts/validator.js
+++ interfaz/scripts/validator.js
@@ -1,11 +1,11 @@
 const requeriments = {
     name: /^[a-zA-Z\s]{2,25}$/,
     lastName: /^[a-zA-Z\s]{2,25}$/,
-    email:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,50}$/,
+    email:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+?.[a-zA-Z]{2,50}$/,
     password:/^[\w\S-¿?^*´!|#$%&/{}()=><;:,.-]{8,50}$/,
-    day: /^[0-9\s]{1,4}$/,
-    month: /^[0-9\s]{1,4}$/,
-    year: /^[0-9\s]{1,4}$/
+    day: /^[0-9\s]{2}$/,
+    month: /^[0-9\s]{2}$/,
+    year: /^[0-9\s]{4}$/
 }
 const tFs = {
     name: false,
@@ -24,9 +24,8 @@ const submit = document.getElementById("signUp");
 inputSignUp.forEach(input => {
     input.addEventListener("input", (e) => {
         let validation = validationInputs(requeriments[e.target.name], e.target.value, e.target.id);
-        validationInputs(requeriments[e.target.name], e.target.value, e.target.id)
         submitValidation(validation);
-    });
+    }); 
 })
 
 function validationInputs(requeriment, input, idInput) {
@@ -34,16 +33,17 @@ function validationInputs(requeriment, input, idInput) {
     const element = document.getElementById(idInput);
     try{
         if(requeriment.test(input)){
-                element.classList.add("valid");
-                element.classList.remove("invalid");
-                tFs[idInput] = true;
-                return true;
-            } else{
-                element.classList.add("invalid");
-                element.classList.remove("valid");
-                tFs[idInput] = false;
-                throw new Error("Ingresa valores validos");
-            }
+            element.classList.add("valid");
+            element.classList.remove("invalid");
+            tFs[idInput] = true;
+                
+        } else{
+            element.classList.add("invalid");
+            element.classList.remove("valid");
+            tFs[idInput] = false;
+            throw new Error("Ingresa valores validos");
+        }
+        return true;
     }catch(e) {
         if(!requeriment.test(input)){
             console.error(e.message);
@@ -52,19 +52,11 @@ function validationInputs(requeriment, input, idInput) {
     }
 }
 
-function submitValidation(validation) {
+function submitValidation() {
     let allElementsValid = true;
-    inputSignUp.forEach( () => {
-        if (validation) {
-            allElementsValid = true;   
-        } else {
-            allElementsValid = false;   
-        }
-    });
-    if (allElementsValid) {
-        submit.removeAttribute("disabled");
-    } else {
-        submit.setAttribute("disabled", "");
+    for(let input in tFs){
+        (!tFs[input]) ? allElementsValid = false : allElementsValid = true;
     }
+        (allElementsValid) ? submit.removeAttribute("disabled") : submit.setAttribute("disabled", "");
 }
 
