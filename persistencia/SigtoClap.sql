ac095d10c7da4ce1825646b71744336336171f6b
Dario Diaz <dariodiaz2006@gmail.com>
2024-09-10 07:40:09 -0300

    Data base updated

---
+++ persistencia/SigtoClap.sql
@@ -0,0 +1,570 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-09-2024 a las 01:08:42
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+CREATE DATABASE sigtoclap;
+USE sigtoclap;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sigto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `agrega`
+--
+
+CREATE TABLE `agrega` (
+  `idProducto` int(11) NOT NULL,
+  `idCarritoCompras` int(11) NOT NULL,
+  `cantidad` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `almacena`
+--
+
+CREATE TABLE `almacena` (
+  `idCarritoCompras` int(11) NOT NULL,
+  `idPedido` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carritodecompras`
+--
+
+CREATE TABLE `carritodecompras` (
+  `idCarritoCompras` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comenta`
+--
+
+CREATE TABLE `comenta` (
+  `idComentario` int(11) NOT NULL,
+  `fechaComentario` date NOT NULL,
+  `idUsuario` int(11) DEFAULT NULL,
+  `idProducto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `consulta`
+--
+
+CREATE TABLE `consulta` (
+  `idUsuario` int(11) NOT NULL,
+  `idHistorial` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contiene`
+--
+
+CREATE TABLE `contiene` (
+  `idProducto` int(11) NOT NULL,
+  `idInventario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empresa`
+--
+
+CREATE TABLE `empresa` (
+  `idEmpresa` int(11) NOT NULL,
+  `nombreEmpresa` varchar(30) NOT NULL,
+  `emailEmpresa` varchar(30) NOT NULL,
+  `telefonoEmpresa` int(11) NOT NULL,
+  `contraseniaEmpresa` varchar(45) NOT NULL,
+  `direccionEmpresa` varchar(250) NOT NULL,
+  `fotoDePerfilEmpresa` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entrega`
+--
+
+CREATE TABLE `entrega` (
+  `idProducto` int(11) NOT NULL,
+  `idPedido` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `formula`
+--
+
+CREATE TABLE `formula` (
+  `idUsuario` int(11) NOT NULL,
+  `idTicket` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `guarda`
+--
+
+CREATE TABLE `guarda` (
+  `idProducto` int(11) NOT NULL,
+  `idHistorial` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial`
+--
+
+CREATE TABLE `historial` (
+  `idHistorial` int(11) NOT NULL,
+  `fechaHistorial` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inventario`
+--
+
+CREATE TABLE `inventario` (
+  `idInventario` int(11) NOT NULL,
+  `idEmpresa` int(11) DEFAULT NULL,
+  `stock` int(11) NOT NULL,
+  `categorias` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `oferta`
+--
+
+CREATE TABLE `oferta` (
+  `idOferta` int(11) NOT NULL,
+  `idProducto` int(11) DEFAULT NULL,
+  `descuento` int(11) NOT NULL,
+  `nombreOferta` varchar(30) NOT NULL,
+  `fechaOferta` enum('Inicio','Fin') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `idPedido` int(11) NOT NULL,
+  `idUsuario` int(11) DEFAULT NULL,
+  `tipo` enum('Delivery','PickUp') NOT NULL,
+  `metodoDePago` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `piden`
+--
+
+CREATE TABLE `piden` (
+  `idEmpresa` int(11) NOT NULL,
+  `idTicket` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `idProducto` int(11) NOT NULL,
+  `nombreProducto` varchar(30) NOT NULL,
+  `precioProducto` int(10) UNSIGNED NOT NULL,
+  `categoriaProducto` varchar(250) NOT NULL,
+  `descripcionProducto` varchar(250) NOT NULL,
+  `valoracionProducto` int(11) NOT NULL,
+  `imagenProducto` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `publica`
+--
+
+CREATE TABLE `publica` (
+  `idEmpresa` int(11) NOT NULL,
+  `idOferta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ticketdeayuda`
+--
+
+CREATE TABLE `ticketdeayuda` (
+  `idTicket` int(11) NOT NULL,
+  `asuntoTicket` varchar(250) NOT NULL,
+  `fechaTicket` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tiene`
+--
+
+CREATE TABLE `tiene` (
+  `idUsuario` int(11) NOT NULL,
+  `idCarritoCompras` int(11) DEFAULT NULL,
+  `fechaCreacion` date DEFAULT NULL,
+  `estado` enum('Vacio','En Curso','En Pago') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `nombreUsuario` varchar(30) NOT NULL,
+  `emailUsuario` varchar(30) NOT NULL,
+  `telefonoUsuario` int(11) NOT NULL,
+  `contraseniaUsuario` varchar(45) NOT NULL,
+  `direccionUsuario` varchar(250) NOT NULL,
+  `fotoDePerfilUsuario` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `agrega`
+--
+ALTER TABLE `agrega`
+  ADD PRIMARY KEY (`idProducto`,`idCarritoCompras`),
+  ADD KEY `idCarritoCompras` (`idCarritoCompras`);
+
+--
+-- Indices de la tabla `almacena`
+--
+ALTER TABLE `almacena`
+  ADD PRIMARY KEY (`idCarritoCompras`,`idPedido`),
+  ADD KEY `idPedido` (`idPedido`);
+
+--
+-- Indices de la tabla `carritodecompras`
+--
+ALTER TABLE `carritodecompras`
+  ADD PRIMARY KEY (`idCarritoCompras`);
+
+--
+-- Indices de la tabla `comenta`
+--
+ALTER TABLE `comenta`
+  ADD PRIMARY KEY (`idComentario`,`idProducto`),
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `idProducto` (`idProducto`);
+
+--
+-- Indices de la tabla `consulta`
+--
+ALTER TABLE `consulta`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD KEY `idHistorial` (`idHistorial`);
+
+--
+-- Indices de la tabla `contiene`
+--
+ALTER TABLE `contiene`
+  ADD PRIMARY KEY (`idProducto`),
+  ADD KEY `idInventario` (`idInventario`);
+
+--
+-- Indices de la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  ADD PRIMARY KEY (`idEmpresa`);
+
+--
+-- Indices de la tabla `entrega`
+--
+ALTER TABLE `entrega`
+  ADD PRIMARY KEY (`idProducto`,`idPedido`),
+  ADD KEY `idPedido` (`idPedido`);
+
+--
+-- Indices de la tabla `formula`
+--
+ALTER TABLE `formula`
+  ADD PRIMARY KEY (`idUsuario`,`idTicket`),
+  ADD KEY `idTicket` (`idTicket`);
+
+--
+-- Indices de la tabla `guarda`
+--
+ALTER TABLE `guarda`
+  ADD PRIMARY KEY (`idProducto`),
+  ADD KEY `idHistorial` (`idHistorial`);
+
+--
+-- Indices de la tabla `historial`
+--
+ALTER TABLE `historial`
+  ADD PRIMARY KEY (`idHistorial`);
+
+--
+-- Indices de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD PRIMARY KEY (`idInventario`),
+  ADD KEY `idEmpresa` (`idEmpresa`);
+
+--
+-- Indices de la tabla `oferta`
+--
+ALTER TABLE `oferta`
+  ADD PRIMARY KEY (`idOferta`);
+
+--
+-- Indices de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`idPedido`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indices de la tabla `piden`
+--
+ALTER TABLE `piden`
+  ADD PRIMARY KEY (`idEmpresa`,`idTicket`),
+  ADD KEY `idTicket` (`idTicket`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`idProducto`);
+
+--
+-- Indices de la tabla `publica`
+--
+ALTER TABLE `publica`
+  ADD PRIMARY KEY (`idOferta`,`idEmpresa`),
+  ADD KEY `idEmpresa` (`idEmpresa`);
+
+--
+-- Indices de la tabla `ticketdeayuda`
+--
+ALTER TABLE `ticketdeayuda`
+  ADD PRIMARY KEY (`idTicket`);
+
+--
+-- Indices de la tabla `tiene`
+--
+ALTER TABLE `tiene`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD KEY `idCarritoCompras` (`idCarritoCompras`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `carritodecompras`
+--
+ALTER TABLE `carritodecompras`
+  MODIFY `idCarritoCompras` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `comenta`
+--
+ALTER TABLE `comenta`
+  MODIFY `idComentario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  MODIFY `idEmpresa` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `historial`
+--
+ALTER TABLE `historial`
+  MODIFY `idHistorial` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  MODIFY `idInventario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `oferta`
+--
+ALTER TABLE `oferta`
+  MODIFY `idOferta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `idPedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ticketdeayuda`
+--
+ALTER TABLE `ticketdeayuda`
+  MODIFY `idTicket` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `agrega`
+--
+ALTER TABLE `agrega`
+  ADD CONSTRAINT `agrega_ibfk_1` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`),
+  ADD CONSTRAINT `agrega_ibfk_2` FOREIGN KEY (`idCarritoCompras`) REFERENCES `carritodecompras` (`idCarritoCompras`);
+
+--
+-- Filtros para la tabla `almacena`
+--
+ALTER TABLE `almacena`
+  ADD CONSTRAINT `almacena_ibfk_1` FOREIGN KEY (`idCarritoCompras`) REFERENCES `carritodecompras` (`idCarritoCompras`),
+  ADD CONSTRAINT `almacena_ibfk_2` FOREIGN KEY (`idPedido`) REFERENCES `pedido` (`idPedido`);
+
+--
+-- Filtros para la tabla `comenta`
+--
+ALTER TABLE `comenta`
+  ADD CONSTRAINT `comenta_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `comenta_ibfk_2` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`);
+
+--
+-- Filtros para la tabla `consulta`
+--
+ALTER TABLE `consulta`
+  ADD CONSTRAINT `consulta_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `consulta_ibfk_2` FOREIGN KEY (`idHistorial`) REFERENCES `historial` (`idHistorial`);
+
+--
+-- Filtros para la tabla `contiene`
+--
+ALTER TABLE `contiene`
+  ADD CONSTRAINT `contiene_ibfk_1` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`),
+  ADD CONSTRAINT `contiene_ibfk_2` FOREIGN KEY (`idInventario`) REFERENCES `inventario` (`idInventario`);
+
+--
+-- Filtros para la tabla `entrega`
+--
+ALTER TABLE `entrega`
+  ADD CONSTRAINT `entrega_ibfk_1` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`),
+  ADD CONSTRAINT `entrega_ibfk_2` FOREIGN KEY (`idPedido`) REFERENCES `pedido` (`idPedido`);
+
+--
+-- Filtros para la tabla `formula`
+--
+ALTER TABLE `formula`
+  ADD CONSTRAINT `formula_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `formula_ibfk_2` FOREIGN KEY (`idTicket`) REFERENCES `ticketdeayuda` (`idTicket`);
+
+--
+-- Filtros para la tabla `guarda`
+--
+ALTER TABLE `guarda`
+  ADD CONSTRAINT `guarda_ibfk_1` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`),
+  ADD CONSTRAINT `guarda_ibfk_2` FOREIGN KEY (`idHistorial`) REFERENCES `historial` (`idHistorial`);
+
+--
+-- Filtros para la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD CONSTRAINT `inventario_ibfk_1` FOREIGN KEY (`idEmpresa`) REFERENCES `empresa` (`idEmpresa`);
+
+--
+-- Filtros para la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Filtros para la tabla `piden`
+--
+ALTER TABLE `piden`
+  ADD CONSTRAINT `piden_ibfk_1` FOREIGN KEY (`idEmpresa`) REFERENCES `empresa` (`idEmpresa`),
+  ADD CONSTRAINT `piden_ibfk_2` FOREIGN KEY (`idTicket`) REFERENCES `ticketdeayuda` (`idTicket`);
+
+--
+-- Filtros para la tabla `publica`
+--
+ALTER TABLE `publica`
+  ADD CONSTRAINT `publica_ibfk_1` FOREIGN KEY (`idEmpresa`) REFERENCES `empresa` (`idEmpresa`),
+  ADD CONSTRAINT `publica_ibfk_2` FOREIGN KEY (`idOferta`) REFERENCES `oferta` (`idOferta`);
+
+--
+-- Filtros para la tabla `tiene`
+--
+ALTER TABLE `tiene`
+  ADD CONSTRAINT `tiene_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `tiene_ibfk_2` FOREIGN KEY (`idCarritoCompras`) REFERENCES `carritodecompras` (`idCarritoCompras`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
