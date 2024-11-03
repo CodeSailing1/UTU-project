d70acced6bff609cdc35a3cefe035a68bb223fd8
Darío Díaz <dariodiaz2006@gmail.com>
2024-11-02 23:16:12 -0300

    Linked all social media accounts with the icons in the footer and made a page and css for terms and services

--- interfaz/public-html/footer.php
+++ interfaz/public-html/footer.php
@@ -1,13 +1,13 @@
 <footer class="py-3 my-4">
         <div>
             <ul class="nav justify-content-center align-items-center border-bottom pb-3 mb-3">
-                <li class="nav-item"><img src="../src/img/svgs/brand-x.svg" alt=""><a href=""></a></img></li>
-                <li class="nav-item"><img src="../src/img/svgs/brand-youtube.svg" alt=""><a href=""></a></img></li>
-                <li class="nav-item"><img src="../src/img/svgs/brand-facebook.svg" alt=""><a href=""></a></img></li>
-                <li class="nav-item"><img src="../src/img/svgs/brand-instagram.svg" alt=""><a href=""></a></img></li>
+                <li class="nav-item"><a href="https://www.youtube.com/channel/UCmeoz8oYrjV_5PBpRjfgxkw"><img src="../src/img/svgs/brand-x.svg" alt=""></img></a></li>
+                <li class="nav-item"><a href="https://x.com/CodeSailing"><img src="../src/img/svgs/brand-youtube.svg" alt=""></img></a></li>
+                <li class="nav-item"><a href="https://www.facebook.com/profile.php?id=61568051164007"><img src="../src/img/svgs/brand-facebook.svg" alt=""></img></a></li>
+                <li class="nav-item"><a href="https://www.instagram.com/codesailing/"><img src="../src/img/svgs/brand-instagram.svg" alt=""></img></a></li>
             </ul>
             <ul class="nav justify-content-center align-items-center border-bottom pb-3">
-                <li class="nav-item"><a class="nav-link px-2 text-muted" href="#" target="_blank">Term of service and use</a></li>
+                <li class="nav-item"><a class="nav-link px-2 text-muted" href="/UTU-project/interfaz/public-html/terminos&condiciones.php" target="_blank">Term of service and use</a></li>
                 <li class="nav-item"><a class="nav-link px-2 text-muted" href="#" target="_blank">How we take care of your privacity</a></li>
                 <li class="nav-item"><a class="nav-link px-2 text-muted" href="#" target="_blank">Contact</a></li>
             </ul>
